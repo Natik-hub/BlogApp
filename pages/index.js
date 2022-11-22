bcbb70d88f98d6c7ef1dd6cb1670d3a66ea50254
Natnael Kibru <nathnaelkibru@gmail.com>
2022-11-22 13:47:27 +0300

    displaypost

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home ({ posts }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12 text-white'>
-          {posts.map((post) => <PostCard post={post} key={post.title}/>)}
+          {posts.map((post) => <PostCard post={post.node} key={post.title}/>)}
       </div>
       <div className='lg:col-span-4 col-span-1'>
             <div className='lg:sticky relative top-8'>
