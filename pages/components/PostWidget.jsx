8082686b7df114429fdbf3ab265970822ead5b84
Natnael Kibru <nathnaelkibru@gmail.com>
2022-11-24 15:51:42 +0300

    categoriesbar

--- pages/components/PostWidget.jsx
+++ pages/components/PostWidget.jsx
@@ -20,7 +20,6 @@ const PostWidget = ({ categories, slug }) => {
   console.log(relatedPosts)
 
   return (
-    <div className='lg:col-span-4 col-span-1'>
       <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">{slug ? 'Related Posts' : 'Recent Posts'}</h3>
       {relatedPosts.map((post, index) => (
@@ -41,8 +40,6 @@ const PostWidget = ({ categories, slug }) => {
         </div>
       ))}
     </div>
-    </div>
-
   )
 };
 
