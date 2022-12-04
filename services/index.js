39703a3ed62b91c8601b17c9d737ed7848c7131b
Natnael Kibru <nathnaelkibru@gmail.com>
2022-12-04 16:23:24 +0300

    servicecheck

--- services/index.js
+++ services/index.js
@@ -3,36 +3,35 @@ import { request, gql } from 'graphql-request';
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
-
   const query = gql`
-  query Assets {
-    postsConnection {
-      edges {
-        node {
-          author {
-            bio
-            name
-            id
-            photo {
+    query Assets {
+      postsConnection {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
               url
             }
-          }
-          createdAt
-          slug
-          title
-          excerpt
-          featuredImage {
-            url
-          }
-          categories {
-            name
-            slug
+            categories {
+              name
+              slug
+            }
           }
         }
       }
     }
-  }
-  
   `;
 
   const result = await request(graphqlAPI, query);
