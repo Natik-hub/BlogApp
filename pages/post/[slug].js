8015d7104f5996948a8387d84dc2b333dd62c890
Natnael Kibru <nathnaelkibru@gmail.com>
2022-12-04 16:09:17 +0300

    slugfix

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
-import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm} from '../../components';
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
 import { getPosts, getPostDetails } from '../../services';
+import { AdjacentPosts } from '../../sections';
 
 const PostDetails = ({ post }) => {
-  console.log(post);
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
 
   return (
+    <>
       <div className="container mx-auto px-10 mb-8">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className="col-span-1 lg:col-span-8">
             <PostDetail post={post} />
             <Author author={post.author} />
+            <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
             <CommentsForm slug={post.slug} />
             <Comments slug={post.slug} />
           </div>
@@ -23,6 +31,7 @@ const PostDetails = ({ post }) => {
           </div>
         </div>
       </div>
+    </>
   );
 };
 export default PostDetails;
